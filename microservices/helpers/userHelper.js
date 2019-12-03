d9079c1b696e935c8d6142b60de6578e6a7be7eb
poonam gawali <poonam.gawali@weboniselab.com>
2019-12-03 16:22:48 +0530

    Resolve eslint error for helper and model file

--- microservices/helpers/userHelper.js
+++ microservices/helpers/userHelper.js
@@ -1,78 +1,77 @@
 var userModel = require('../models/user.model');
-var config = require('../config/database.config');
 
 exports.findAll = () => {
-    return new Promise((resolve,reject)=>{
-        userModel.find().then(data=>{
-            resolve(data);
-        }).catch(err=>{data
-            reject(err);
-        });
+  return new Promise((resolve, reject) => {
+    userModel.find().then((data) => {
+      resolve(data);
+    }).catch((err) => {
+      reject(err);
     });
-}
+  });
+};
 
 exports.findOne = (userId) => {
-    return new Promise((resolve,reject)=>{
-        userModel.findById(userId).then(data=>{
-            resolve(data);
-        }).catch(err=>{data
-            reject(err);
-        });
+  return new Promise((resolve, reject) => {
+    userModel.findById(userId).then((data) => {
+      resolve(data);
+    }).catch((err) => {
+      reject(err);
     });
-}
+  });
+};
 
-exports.create = (data) => {
-    return new Promise((resolve,reject)=>{
-        //Create a User
-        const User = new userModel(data);
-        //Save User in the database
-        User.save().then(data=>{
-            resolve(data);
-        }).catch(err=>{
-            reject(err);
-        });
+exports.create = (userData) => {
+  return new Promise((resolve, reject) => {
+  // Create a User
+    const User = new userModel(userData);
+    // Save User in the database
+    User.save().then((data) => {
+      resolve(data);
+    }).catch((err) => {
+      reject(err);
     });
-}
+  });
+};
 
-exports.update = (userId,data) => {
-    return new Promise((resolve,reject)=>{
-        //Update User in the database
-        userModel.findByIdAndUpdate(userId,data,{new:true}).then(data=>{
-            resolve(data);
-        }).catch(err=>{
-            reject(err);
-        });
+exports.update = (userId, userData) => {
+  return new Promise((resolve, reject) => {
+    // Update User in the database
+    userModel.findByIdAndUpdate(userId, userData, { new: true }).then((data) => {
+      resolve(data);
+    }).catch((err) => {
+      reject(err);
     });
-}
+  });
+};
 
-exports.patch = (userId,data) => {
-    return new Promise((resolve,reject)=>{
-        userModel.findByIdAndUpdate(userId,data,{new:true}).then(data=>{
-            resolve(data);
-        }).catch(err=>{
-            reject(err);
-        });
+exports.patch = (userId, userData) => {
+  return new Promise((resolve, reject) => {
+    userModel.findByIdAndUpdate(userId, userData, { new: true }).then((data) => {
+      resolve(data);
+    }).catch((err) => {
+      reject(err);
     });
-}
+  });
+};
 
 exports.deleteOne = (userId) => {
-    return new Promise((resolve,reject)=>{
-        //Delete User in the database
-        userModel.findByIdAndRemove(userId).then(data=>{
-            resolve(data);
-        }).catch(err=>{
-            reject(err);
-        });
+  return new Promise((resolve, reject) => {
+  // Delete User in the database
+    userModel.findByIdAndRemove(userId).then((data) => {
+      resolve(data);
+    }).catch((err) => {
+      reject(err);
     });
-}
+  });
+};
 
-exports.deleteMany = function(userIds){
-    return new Promise((resolve,reject)=>{
-        //Delete User in the database
-        userModel.remove({_id:userIds}).then(data=>{
-            resolve(data);
-        }).catch(err=>{
-            reject(err);
-        });
+exports.deleteMany = (userIds) => {
+  return new Promise((resolve, reject) => {
+  // Delete User in the database
+    userModel.remove({ _id: userIds }).then((data) => {
+      resolve(data);
+    }).catch((err) => {
+      reject(err);
     });
-}
+  });
+};
--- microservices/models/user.model.js
+++ microservices/models/user.model.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
+const { Schema } = mongoose.Schema;
 var userSchema = new Schema({
-    username : {type:String,unique:true},
-    password : {type:String},
-    firstname : {type:String},
-    lastname : {type:String},
-    email : {type:String}
+  username: { type: String, unique: true },
+  password: { type: String },
+  firstname: { type: String },
+  lastname: { type: String },
+  email: { type: String }
 });
 
-var Login = mongoose.model('userDetails',userSchema);
+var Login = mongoose.model('userDetails', userSchema);
 module.exports = Login;
